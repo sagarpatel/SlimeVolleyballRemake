902a574d37d1eaa698ccdde5e8665ef1d5d46af4
Sagar Patel <17.sagar.patel@gmail.com>
2014-06-26 02:44:54 +0900

    Added physics based player controls, still needs work

---
+++ SVR_UnityFolder/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour 
+{
+	public float xMoveScale = 1.0f;
+	public float yMoveScale = 1.0f;
+
+	Rigidbody2D playerRigidbody;
+
+	// Use this for initialization
+	void Start () 
+	{
+		playerRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float xForce = 0;
+		float yForce = 0;
+		if(Input.GetKeyDown(KeyCode.A))
+			xForce -= xMoveScale * Time.deltaTime;
+		if(Input.GetKeyDown(KeyCode.D))
+			xForce += xMoveScale * Time.deltaTime;
+
+		if(Input.GetKeyDown(KeyCode.S))
+			yForce -= yMoveScale * Time.deltaTime;
+		if(Input.GetKeyDown(KeyCode.W))
+			yForce += yMoveScale * Time.deltaTime;
+
+
+		print(xForce);
+
+		playerRigidbody.AddForce(new Vector3(xForce, yForce, 0));
+
+
+	}
+}
